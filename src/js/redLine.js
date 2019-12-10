801bfa40b24fdbc4a0efe59dec3cd0a79fc48939
Denis <krollik1000000@gmail.com>
2019-12-10 17:51:17 +0200

    redLine

--- src/js/redLine.js
+++ src/js/redLine.js
@@ -1,2 +1,15 @@
-import {createWeek} from './createWeek.js'
 
+
+// const red = document.querySelector('.redLine');
+// let now = new Date();
+
+// export function line () {
+//     let hour = now.getHours() * 60 + now.getMinutes();
+//     let heightOneMinute = 42 / 60;
+//     red.style.top = (hour * heightOneMinute) + 'px'
+// }
+
+// line();
+// setInterval(() => {
+//     line();
+// },1000);
