80f6d014a55c1a8c659d1f701a264d1572c1ccff
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-10 18:35:56 +0200

    formatting

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import { createElements } from './functions.js'
-import { days } from './days.js'
-import { today } from './days.js'
+import { createElements } from './functions.js';
+import { days } from './days.js';
+import { today } from './days.js';
 import { addContentSidebar } from './createSidebar.js';
 import { showPopupWindow } from './showPopupWindow.js';
 import { createEvent } from './createEvent.js';
 import { routingWeeks } from './routingWeeks.js';
 import { deleteObjectEvent } from './deleteEvent.js';
-import { nameDay } from './days.js'
-import { right } from './showYearAndMonth.js'
-import { left } from './showYearAndMonth.js'
-import { btnToday } from './showYearAndMonth.js'
+import { nameDay } from './days.js';
+import { right } from './showYearAndMonth.js';
+import { left } from './showYearAndMonth.js';
+import { btnToday } from './showYearAndMonth.js';
 
 addContentSidebar();
 showPopupWindow();
