c7fd3892da7769d9c166bb308824258434fd2e5c
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-23 20:15:12 +0200

    fix bug in select time

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,13 +1,22 @@
-function updateLocalStorage(key, value) {
+const daseUrl = 'https://crudcrud.com/api/ad3c49008aa24b7db73bc2572e4f6f28/data';
+
+function updateStorage(key, value) {
+    // return fetch(baseUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json;charset=utf-8',
+    //     },
+    //     body: JSON.stringify(taskData),
+    // })
     localStorage.setItem(key, JSON.stringify(value));
 };
 
-function getItemLocalStorage(key) {
+function getItemStorage(key) {
     return JSON.parse(localStorage.getItem(key), reviver);
 };
 
 function reviver(key, value) {
-    if(['createDate', 'startDate', 'endDate'].includes(key)) {
+    if (['createDate', 'startDate', 'endDate'].includes(key)) {
         return new Date(value);
     }
 
@@ -15,18 +24,18 @@ function reviver(key, value) {
 };
 
 function setShowedMonday(showedMonday) {
-    updateLocalStorage('showedMonday', showedMonday);
+    updateStorage('showedMonday', showedMonday);
 };
 
 function getShowedMonday() {
-    if (getItemLocalStorage('showedMonday')) {
-        return new Date(getItemLocalStorage('showedMonday'));
+    if (getItemStorage('showedMonday')) {
+        return new Date(getItemStorage('showedMonday'));
     }
-    return getItemLocalStorage('showedMonday');
+    return getItemStorage('showedMonday');
 };
 
 function getEvents() {
-    return getItemLocalStorage('listEvents') || [];
+    return getItemStorage('listEvents') || [];
 };
 
 function addEvent(event) {
@@ -40,7 +49,7 @@ function addEvent(event) {
         description: event.description,
         color: event.color,
     });
-    updateLocalStorage('listEvents', listEvents)
+    updateStorage('listEvents', listEvents)
 };
 
 function getEventById(idEvent) {
@@ -58,7 +67,49 @@ function deleteEvent(idEvent) {
     });
     listEvents.splice(indexEvent, 1);
 
-    updateLocalStorage('listEvents', listEvents)
+    updateStorage('listEvents', listEvents)
 };
 
 export { getEvents, getEventById, addEvent, deleteEvent, getShowedMonday, setShowedMonday };
+
+
+
+// const baseUrl = 'https://crudcrud.com/api/ce9a261481894ed4a43d1236fc4ca3d9/tasks';
+
+// function mapTasks(tasks) {
+//     return tasks.map(({ _id, ...rest }) => ({ ...rest, id: _id }));
+// }
+
+// function getTasksList() {
+//     return fetch(baseUrl)
+//         .then(response => response.json())
+//         .then(tasks => mapTasks(tasks))
+// };
+
+// function creatTask(taskData) {
+//     return fetch(baseUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(taskData),
+//     })
+// };
+
+// function updateTask(taskId, updatedTaskData) {
+//     return fetch(`${baseUrl}/${taskId}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(updatedTaskData),
+//     })
+// };
+
+// function deleteTask(taskId) {
+//     return fetch(`${baseUrl}/${taskId}`, {
+//         method: 'DELETE',
+//     })
+// };
+
+// export { getTasksList, creatTask, updateTask, deleteTask };
